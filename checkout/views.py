b7cc988885d3882c44f0cd01beeb4add64d4492a
JosefinE7 <fin.jos@hotmail.com>
2020-12-28 16:41:44 +0000

    Added Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51I3NxrAvHz7FKrrcnQYKvCAWJKOwKyxVFgKOo8EvERG2VwhGeHlqacH9969mJ40d7BhxjOQFlotT2imMk04fJ46M00SWywstoY',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
