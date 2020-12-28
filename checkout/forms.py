723fba1a91d528144236c0eb89788d0f33b989e1
JosefinE7 <fin.jos@hotmail.com>
2020-12-28 13:32:16 +0000

    Added checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,7 +15,7 @@ class OrderForm(forms.ModelForm):
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
-        super().__init__.(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
