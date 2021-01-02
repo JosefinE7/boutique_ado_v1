fdd20bd51f98c113e0f9ccfda366d145601465bb
JosefinE7 <fin.jos@hotmail.com>
2021-01-02 18:11:26 +0000

    Fix stripe_elements payment and shipping county to state

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -79,7 +79,7 @@ form.addEventListener('submit', function (ev) {
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
-                        county: $.trim(form.county.value),
+                        state: $.trim(form.county.value),
 
                     }
                 }
@@ -93,7 +93,7 @@ form.addEventListener('submit', function (ev) {
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     postal_code: $.trim(form.postcode.value),
-                    county: $.trim(form.county.value),
+                    state: $.trim(form.county.value),
 
                 }
             },
